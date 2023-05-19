3287efa4fb7f100873c78d1bdd5141bedcb4ef56
davidka2003 <d4v1ds0n.p@gmail.com>
2023-05-20 00:14:54 +0300

    +developers

---
+++ users/users.tf
@@ -0,0 +1,61 @@
+resource "aws_iam_group" "developer" {
+  name = "developer-group"
+
+}
+
+resource "aws_iam_group_policy_attachment" "developer-ecr" {
+  group      = aws_iam_group.developer.name
+  policy_arn = aws_iam_policy.allow-pull-push-ecr.arn
+}
+
+resource "aws_iam_group_policy_attachment" "developer-ecs" {
+  group      = aws_iam_group.developer.name
+  policy_arn = aws_iam_policy.allow-update-ecs-deployment.arn
+}
+
+resource "aws_iam_policy" "allow-pull-push-ecr" {
+  name        = "pull-push-ecr"
+  description = "Allow pull and push to ecr for developers"
+  policy      = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:CompleteLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:InitiateLayerUpload",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:GetAuthorizationToken"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_policy" "allow-update-ecs-deployment" {
+  name        = "update-ecs-deployment"
+  description = "Allow create new deployments for ecs services"
+  policy      = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ecs:UpdateService"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+  EOF
+}
