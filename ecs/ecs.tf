c34cda8a3e3427cb18b61154b752a5eb76f5024f
davidka2003 <d4v1ds0n.p@gmail.com>
2023-05-20 22:25:20 +0300

    cost reduction ecs

--- ecs/ecs.tf
+++ ecs/ecs.tf
@@ -74,8 +74,8 @@ resource "aws_ecs_task_definition" "this" {
   ])
   execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn      = aws_iam_role.ecs_task_execution_role.arn
-  cpu                = "512"
-  memory             = "1024"
+  cpu                = "256"
+  memory             = "512"
   network_mode       = "awsvpc"
 }
 
